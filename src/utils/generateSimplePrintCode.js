9e1b47bbe2f147fd3dcc3efb9e71a03dfc3d1818
Antonio Costa <juninhomathoni99@gmail.com>
2021-09-11 12:04:15 -0300

    NewMeasuresForPrint

--- src/utils/generateSimplePrintCode.js
+++ src/utils/generateSimplePrintCode.js
@@ -1,16 +1,16 @@
 export function generateSimplePrintCode(data) {
   const printItens = data.map(item => {
     return `
-      ^XA
-  
-      ^FO125,25^GB570,255,8^FS
-      ^FO140,55^A0,55^FD ${item.PRODUTO}^FS
-      )}^FS
-      ^FO140,145^A0,35^FD ${item.DESCRICAO.slice(0, 29)}^FS
-      ^FO140,185^A0,35^FD ${item.DESCRICAO.slice(30, 59)}^FS
-      ^FO140,225^A0,35^FD ${item.DESCRICAO.slice(60, 89)}^FS
-  
-      ^XZ
+    ^XA
+    
+    ^FO20,25^GB590,255,8^FS
+    ^FO35,55^A0,55^FD ${item.PRODUTO}^FS
+    )}^FS
+    ^FO35,145^A0,35^FD ${item.DESCRICAO.slice(0, 30)}^FS
+    ^FO35,185^A0,35^FD ${item.DESCRICAO.slice(30, 60)}^FS
+    ^FO35,225^A0,35^FD ${item.DESCRICAO.slice(60, 89)}^FS
+
+    ^XZ
       `.repeat(item.SALDO);
   });
   return printItens;
