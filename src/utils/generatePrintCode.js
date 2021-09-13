df0824af748e0b17943dae57ccbf68e74c3a2732
Antonio Costa <juninhomathoni99@gmail.com>
2021-09-13 09:43:44 -0300

    NewMeasuresForPrint02

--- src/utils/generatePrintCode.js
+++ src/utils/generatePrintCode.js
@@ -3,15 +3,15 @@ export function generatePrintCode(data) {
     return `
       ^XA
   
-      ^FO20,25^GB590,255,8^FS
-      ^FO35,55^A0,55^FD ${item.PRODUTO}^FS
-      ^FO480,55^A0,25^FD PC ${item.PEDIDO}^FS
-      ^FO480,85^A0,25^FD ${new Intl.DateTimeFormat('pt-BR').format(
+      ^FO120,30^GB605,240,8^FS
+      ^FO135,90^A0,55^FD ${item.PRODUTO}^FS
+      ^FO500,90^A0,25^FD PC ${item.PEDIDO}^FS
+      ^FO500,120^A0,25^FD ${new Intl.DateTimeFormat('pt-BR').format(
         Date.now(),
       )}^FS
-      ^FO35,145^A0,35^FD ${item.DESCRICAO.slice(0, 30)}^FS
-      ^FO35,185^A0,35^FD ${item.DESCRICAO.slice(30, 60)}^FS
-      ^FO35,225^A0,35^FD ${item.DESCRICAO.slice(60, 89)}^FS
+      ^FO135,180^A0,35^FD ${item.DESCRICAO.slice(0, 30)}^FS
+      ^FO135,220^A0,35^FD ${item.DESCRICAO.slice(30, 60)}^FS
+      ^FO135,270^A0,35^FD ${item.DESCRICAO.slice(60, 89)}^FS
   
       ^XZ
       `.repeat(item.SALDO);
--- src/utils/generateSimplePrintCode.js
+++ src/utils/generateSimplePrintCode.js
@@ -3,12 +3,12 @@ export function generateSimplePrintCode(data) {
     return `
     ^XA
     
-    ^FO20,25^GB590,255,8^FS
-    ^FO35,55^A0,55^FD ${item.PRODUTO}^FS
+    ^FO120,30^GB605,240,8^FS
+    ^FO135,90^A0,55^FD ${item.PRODUTO}^FS
     )}^FS
-    ^FO35,145^A0,35^FD ${item.DESCRICAO.slice(0, 30)}^FS
-    ^FO35,185^A0,35^FD ${item.DESCRICAO.slice(30, 60)}^FS
-    ^FO35,225^A0,35^FD ${item.DESCRICAO.slice(60, 89)}^FS
+    ^FO135,180^A0,35^FD ${item.DESCRICAO.slice(0, 30)}^FS
+    ^FO135,220^A0,35^FD ${item.DESCRICAO.slice(30, 60)}^FS
+    ^FO135,270^A0,35^FD ${item.DESCRICAO.slice(60, 89)}^FS
 
     ^XZ
       `.repeat(item.SALDO);
