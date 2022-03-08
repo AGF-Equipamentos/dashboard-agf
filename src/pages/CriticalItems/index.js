5949a17052eb2317252f23471b658ebb6b467456
Alana Beatriz <alanabeatrizcourela931@gmail.com>
2022-03-08 17:55:47 -0300

    Index CriticalItems

--- src/pages/CriticalItems/index.js
+++ src/pages/CriticalItems/index.js
@@ -9,46 +9,53 @@ import {
   Table,
   Form,
   FormControl,
-  Spinner
-
+  Spinner,
+  DropdownButton,
+  Dropdown
+  
 } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Container as Cont } from './styles';
 import axios from 'axios';
 
- 
-
 export default function CriticalItems() {
-  
-  const [searchValue, setSearchValue] = useState('');
   const [searchPlaceholder, setSearchPlaceholder] = useState('Pesquise por um código...');
   const [items, setItems] = useState([]);
-  
+  const [filter, setFilter] = useState('Pesquisar...');
+ 
   async function handleSubmit(){
+  let response;
     setSearchPlaceholder(
-     <Spinner animation="border" size="sm" variant="warning" />,
-    );
+      <Spinner animation="border" size="sm" variant="warning" />,
+      );
+      const part_numberInformation = await axios.get(
+        `${process.env.REACT_APP_LOCALHOST}/critical-items`,
+      );
+      setItems(part_numberInformation.data); 
 
-     const part_numberInformation = await axios.get(
-      `${process.env.LOCALHOST}/critical-items`,
-     );
-     setItems(part_numberInformation.data); 
+      if (part_numberInformation.data.length === 0){
+        setSearchPlaceholder('Não tem essa peça...');
 
-  }
-   function keyPressed(event){
-    if (event.key === 'Enter'){
-      handleSubmit();
+      }
+      setItems(part_numberInformation.data);
+
     }
-  }
+    function keyPressed(event){
+      if (event.key === 'Enter'){
+        handleSubmit();
+      }
+    }
     return (
-    <Cont>
-      <Container fluid className="justify-content-center">
+      <Cont>
+        <Container fluid className="justify-content-center">
         <Row>
           <Col align="left" style={{ marginBottom: -50, marginTop: 12 }}>
             <Link
+
               to={{
-                pathname: '/',
+                pathname: '/'
+                
               }}
             >
               <FiArrowLeft color="#999" />
@@ -64,47 +71,66 @@ export default function CriticalItems() {
           <Form.Control type="text" placeholder="Observação de Compras..."/><br />
           <Form.Control type="text" placeholder="Usado..."/><br />
           <Form.Control type="text" placeholder="Responsavel..."/><br />
+              <DropdownButton
+              as={InputGroup.Append}
+              variant="outline-warning"
+              title={filter}
+              id="input-group-dropdown-2"
+              >
+          <Dropdown.Item onClick={() => setFilter('Número da Peça')}>
+              Número da Peça
+          </Dropdown.Item>
+          </DropdownButton>
+            <InputGroup.Append>
           
-          <Button
-          onClick={handleSubmit}
-          type="submit"
-          variant="outline-warning"
-          >
-          Cadastrar 
-          </Button>
+              <Button 
+              onClick={handleSubmit}
+              type="submit"
+              variant="outline-warning"
+              >
+               Cadastar
+              </Button>
+              </InputGroup.Append>
         </InputGroup>
+
         <Table responsive striped bordered houver>
-          <thead>
-            <tr>
-              <th>NÚMERO DA PEÇA</th>
-              <th>DESCRIÇÃO DO ITEM</th>
-              <th>OBSERVAÇÃO DO ESTOQUE</th>
-              <th>OBSERVAÇÃO DE COMPRAS</th>
-              <th>Usado</th>
-              <th>RESPONSAVEL</th>
-              <th>CRIADO:</th>
-              <th>ULT. ATUAL.</th>
-            </tr>
+
+        <thead>
+          <tr>
+            <th>NÚMERO DA PEÇA</th>
+            <th>DESCRIÇÃO DO ITEM</th>
+            <th>OBSERVAÇÃO DO ESTOQUE</th>
+            <th>OBSERVAÇÃO DE COMPRAS</th>
+            <th>Usado</th>
+            <th>RESPONSAVEL</th>
+            <th>CRIADO:</th>
+            <th>ULT. ATUAL.</th>
+          </tr>
           </thead>
+          
           <tbody>
             {items.length !== 0 ? (
               items.map(item => (
-                <tr key={item.NUMERO_PECA}>
-                  <td>{item.NUMERO_PECA}</td>
-                  <td>{item.DESSCRICAO}</td>
-                  <td>{item.OBSERVACAO_ESTOQUE}</td>
-                  <td>{item.OBSERVACAO_COMPRAS}</td>
-                  <td>{item.USADO}</td>
-                  <td>{item.RESPONSAVEL}</td>
-                  <td>{item.CRIADO}</td>
-                  <td>{item.ULT_ATUAL}</td>
+                <tr key={item.part_number}>
+                  <td>{item.part_number}</td>
+                  <td>{item.description}</td>
+                  <td>{item.stock_obs}</td>
+                  <td>{item.purchase_obs}</td>
+                  <td>{item.used_obs}</td>
+                  <td>{item.responsable}</td>
+                  <td>{item.created_at}</td>
+                  <td>{item.update_at}</td>
+                
+
                 </tr>
               ))
             ) : (
+              
               <tr>
-                <td colSpan="8">{searchPlaceholder}</td>
+              <td colSpan="8">{searchPlaceholder}</td>
               </tr>
             )}
+          
           </tbody>
           </Table>
       </Container>
