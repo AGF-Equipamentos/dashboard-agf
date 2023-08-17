bcea3fb10a1679a3a1e736e6012671eb35eeaa00
Ronaldo Rodrigues <ronaldo.rodrigues@agfequipamentos.com>
2023-08-17 12:05:03 -0300

    update_create_button_download

--- src/pages/Kardex/index.js
+++ src/pages/Kardex/index.js
@@ -11,9 +11,8 @@ import {
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Container as Cont } from './styles'
-import DownloadExcelKardexModal from '../../components/DownloadKardexModal'
-
 import api from '../../services/api'
+import { exportToXlsx } from '../../utils/exportToXlsx'
 
 export default function Kardex() {
   const [filialValue, setFilialValue] = useState('')
@@ -23,20 +22,9 @@ export default function Kardex() {
   const [armazemInicioValue, setArmazemInicioValue] = useState('')
   const [armazemFimValue, setArmazemFimValue] = useState('')
   const [kardexRows, setKardexRows] = useState([])
-  const [showDownloadExcelKardexModal, setShowDownloadExcelKardexModal] =
-    useState(false)
-  const [filter, setFilter] = useState('Código')
-  const [searchValue, setSearchValue] = useState('')
   const [searchPlaceholder, setSearchPlaceholder] = useState(
     'Pesquise por um código...'
   )
-
-  async function handleOpenDownloadExcelKardex() {
-    setShowDownloadExcelKardexModal(true)
-  }
-  async function handleCloseDownloadExcelKardex() {
-    setShowDownloadExcelKardexModal(false)
-  }
 
   async function handleSubmit() {
     setSearchPlaceholder(
@@ -79,12 +67,6 @@ export default function Kardex() {
 
   return (
     <Cont>
-      <DownloadExcelKardexModal
-        isOpen={showDownloadExcelKardexModal}
-        handleClose={handleCloseDownloadExcelKardex}
-        filter={filter}
-        searchValue={searchValue}
-      />
       <Container fluid>
         <Row>
           <Col align="left" style={{ marginBottom: -50, marginTop: 12 }}>
@@ -101,7 +83,9 @@ export default function Kardex() {
         <Container fluid className="mb-4">
           <Col className="d-flex justify-content-end">
             <Button
-              onClick={handleOpenDownloadExcelKardex}
+              onClick={() =>
+                exportToXlsx([{ nome: 'Arthur' }, { nome: 'Ronaldo' }], 'nomes')
+              }
               type="submit"
               variant="outline-warning"
             >
@@ -199,17 +183,11 @@ export default function Kardex() {
                 <th>TES/TM</th>
                 <th>CFOP</th>
                 <th>ARMAZEM</th>
-                {/* <th>SERIE</th> */}
                 <th>DOCUMENTO / SERIE</th>
                 <th>IDENT</th>
-                {/* <th>LOJA</th> */}
-                {/* <th>NOME</th> */}
-                {/* <th>NF ORIGEM</th> */}
                 <th>QTDE.</th>
                 <th>CUSTO UNITARIO</th>
                 <th>CUSTO TOTAL</th>
-                {/* <th>SEQUENCIA</th> */}
-                {/* <th>SEQUEN. CALC.</th> */}
                 <th>USUARIO</th>
                 <th>SALDO QTDE.</th>
                 <th>SALDO CUSTO TOTAL</th>
@@ -234,15 +212,11 @@ export default function Kardex() {
                     <td>{product['TES/TM']}</td>
                     <td>{product['CFOP']}</td>
                     <td>{product['Armazem']}</td>
-                    {/*<td>{product['Serie']}</td>*/}
                     <td>
                       {product['Documento']}
                       {product['Serie'] && `/ ${product['Serie']}`}
                     </td>
                     <td>{product['Ident']}</td>
-                    {/* <td>{product['Loja']}</td> */}
-                    {/* <td>{product['Nome']}</td> */}
-                    {/* <td>{product['NF Origem']}</td> */}
                     <td>
                       {product['Qtde.'] < 0 ? (
                         <p className="text-danger">{product['Qtde.']}</p>
@@ -280,8 +254,6 @@ export default function Kardex() {
                         }).format(new Number(product['Custo Total']))
                       )}
                     </td>
-                    {/* <td>{product['Sequencia']}</td> */}
-                    {/* <td>{product['Seq. Calc.']}</td> */}
                     <td>{product['Usuario']}</td>
                     <td>{product['saldoQtde']}</td>
                     <td>
