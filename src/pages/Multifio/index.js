9763d950f6bde4d68ffa22b53c46125b8f92d34d
ronaldoTIrodrigues <rrodriguesdelimafilho@gmail.com>
2023-12-01 17:46:03 -0300

    feat_UpdatePageMultifio

--- src/pages/Multifio/index.js
+++ src/pages/Multifio/index.js
@@ -21,105 +21,92 @@ export default function Multifio() {
           </Col>
         </Row>
         <h1>Multifio</h1>
-        <ProductStockTable products={['MW74V', 'MW86V', 'MW06V']} />
-        <h4>ESTOQUE REDUTORES E MOTORES</h4>
+        <ProductStockTable products={['MW86V', 'MW76V', 'MW74V', 'MW06V']} />
+        <h4>ESTOQUE MOTORES ELÉTRICOS</h4>
+        <PartnumberStockTable
+          products={[
+            { product: 'MW74V', partNumber: '6800000891' },
+            { product: 'MW76V', partNumber: '6800000891' },
+            { product: 'MW86V', partNumber: '6800000891' },
+            { product: 'MW06V', partNumber: '6800000357' },
+            { product: 'MW74V', partNumber: 'VIXMOT0003' },
+            { product: 'MW76V', partNumber: 'VIXMOT0003' },
+            { product: 'MW86V', partNumber: 'VIXMOT0003' }
+          ]}
+        />
+        <h4>ESTOQUE SERVOCONVERSORES</h4>
+        <PartnumberStockTable
+          products={[
+            { product: 'MW74V', partNumber: '6800000823' },
+            { product: 'MW76V', partNumber: '6800000823' },
+            { product: 'MW86V', partNumber: '6800000823' },
+            { product: 'MW06V', partNumber: '6800000823' },
+            { product: 'MW74V', partNumber: '6800000822' },
+            { product: 'MW76V', partNumber: '6800000822' },
+            { product: 'MW86V', partNumber: '6800000822' },
+            { product: 'MW06V', partNumber: '6800000822' }
+          ]}
+        />
+        <h4>ESTOQUE REDUTORES</h4>
         <PartnumberStockTable
           products={[
             { product: 'MW74V', partNumber: '9900002352' },
-            { product: 'MW74V', partNumber: '9900002353' },
+            { product: 'MW76V', partNumber: '9900002352' },
+            { product: 'MW86V', partNumber: '9900002352' },
+            { product: 'MW06V', partNumber: '9900002352' },
+            { product: 'MW06V', partNumber: '9900003017' },
+            { product: 'MW06V', partNumber: '9900003018' },
             { product: 'MW74V', partNumber: '9900002354' },
+            { product: 'MW76V', partNumber: '9900002354' },
+            { product: 'MW86V', partNumber: '9900002354' },
             { product: 'MW74V', partNumber: '9900002355' },
-            { product: 'MW74V', partNumber: 'VIXMOT0003' },
-            { product: 'MW74V', partNumber: '6800000768' },
-            { product: 'MW74V', partNumber: '6800000771' }
+            { product: 'MW76V', partNumber: '9900002355' },
+            { product: 'MW86V', partNumber: '9900002355' },
+            { product: 'MW74V', partNumber: '9900002353' },
+            { product: 'MW76V', partNumber: '9900002353' },
+            { product: 'MW86V', partNumber: '9900002353' },
+            { product: 'MW06V', partNumber: '9900000622' }
           ]}
         />
-        <h4>ESTOQUE FERRO FUNDIDO</h4>
+        <h4>ESTOQUE INVERSORES</h4>
         <PartnumberStockTable
           products={[
-            { product: 'MW74V', partNumber: '1004000077' },
-            { product: 'MW74V', partNumber: '1004000083' },
-            { product: 'MW74V', partNumber: '1004000084' },
-            { product: 'MW74V', partNumber: '1004000085' },
-            { product: 'MW74V', partNumber: '1004000086' },
-            { product: 'MW74V', partNumber: '1004000087' },
-            { product: 'MW74V', partNumber: '1004000088' },
-            { product: 'MW74V', partNumber: '1004000089' },
-            { product: 'MW74V', partNumber: '1004000090' },
-            { product: 'MW74V', partNumber: '1004000091' },
-            { product: 'MW74V', partNumber: '1004000092' },
-            { product: 'MW74V', partNumber: '1004000093' },
-            { product: 'MW74V', partNumber: '1004000094' },
-            { product: 'MW74V', partNumber: '1004000095' },
-            { product: 'MW74V', partNumber: '1004000096' },
-            { product: 'MW74V', partNumber: '1004000097' },
-            { product: 'MW74V', partNumber: '1004000098' },
-            { product: 'MW74V', partNumber: '1004000099' },
-            { product: 'MW74V', partNumber: '1004000100' },
-            { product: 'MW74V', partNumber: '1004000101' },
-            { product: 'MW74V', partNumber: '1004000113' },
-            { product: 'MW74V', partNumber: '1004000114' }
-          ]}
-        />
-        <h4>ESTOQUE ALUMINIO FUNDIDO</h4>
-        <PartnumberStockTable
-          products={[
-            { product: 'MW74V', partNumber: '1004000061' },
-            { product: 'MW74V', partNumber: '1004000062' },
-            { product: 'MW74V', partNumber: '1004000063' },
-            { product: 'MW74V', partNumber: '1004000064' },
-            { product: 'MW74V', partNumber: '1004000066' },
-            { product: 'MW74V', partNumber: '1004000067' },
-            { product: 'MW74V', partNumber: '1004000068' },
-            { product: 'MW74V', partNumber: '1004000069' },
-            { product: 'MW74V', partNumber: '1004000070' },
-            { product: 'MW74V', partNumber: '1004000071' },
-            { product: 'MW74V', partNumber: '1004000072' },
-            { product: 'MW74V', partNumber: '1004000106' },
-            { product: 'MW74V', partNumber: '1004000107' },
-            { product: 'MW74V', partNumber: '1004000108' }
+            { product: 'MW74V', partNumber: '6800000756' },
+            { product: 'MW76V', partNumber: '6800000756' },
+            { product: 'MW86V', partNumber: '6800000756' },
+            { product: 'MW74V', partNumber: '6800000751' },
+            { product: 'MW76V', partNumber: '6800000751' },
+            { product: 'MW06V', partNumber: '6800000450' },
+            { product: 'MW86V', partNumber: '6800000890' }
           ]}
         />
         <h4>ESTOQUE OUTROS </h4>
         <PartnumberStockTable
           products={[
-            { product: 'MW74V', partNumber: '1016000062' },
-            { product: 'MW74V', partNumber: '1016000063' },
-            { product: 'MW74V', partNumber: '6800000772' },
-            { product: 'MW74V', partNumber: 'MW74V30763' },
-            { product: 'MW74V', partNumber: 'MW74V30764' },
-            { product: 'MW74V', partNumber: 'MW74V30765' },
-            { product: 'MW74V', partNumber: 'MW74V30766' },
-            { product: 'MW74V', partNumber: 'MW74V30767' },
-            { product: 'MW74V', partNumber: '9900001623' },
-            { product: 'MW74V', partNumber: '9900002324' },
-            { product: 'MW74V', partNumber: '9900002325' },
-            { product: 'MW74V', partNumber: '9900002326' },
-            { product: 'MW74V', partNumber: '9900002327' },
-            { product: 'MW74V', partNumber: '9900002328' },
-            { product: 'MW74V', partNumber: '9900002329' },
-            { product: 'MW74V', partNumber: 'VIXROL0067' },
-            { product: 'MW74V', partNumber: '9900000876' },
-            { product: 'MW74V', partNumber: '9900002338' },
-            { product: 'MW74V', partNumber: '9900002339' },
-            { product: 'MW74V', partNumber: '9900002340' },
-            { product: 'MW74V', partNumber: '9900002341' },
-            { product: 'MW74V', partNumber: '9900002342' },
-            { product: 'MW74V', partNumber: '9900000877' },
-            { product: 'MW74V', partNumber: '9900002333' },
-            { product: 'MW74V', partNumber: '9900002334' },
-            { product: 'MW74V', partNumber: '9900002335' },
-            { product: 'MW74V', partNumber: '9900002336' },
-            { product: 'MW74V', partNumber: '9900002337' },
-            { product: 'MW74V', partNumber: '9900002029' },
-            { product: 'MW74V', partNumber: '9900002330' },
-            { product: 'MW74V', partNumber: '9900002351' },
+            { product: 'MW74V', partNumber: '9900003309' },
+            { product: 'MW06V', partNumber: '9900003012' },
+            { product: 'MW76V', partNumber: '9900003309' },
+            { product: 'MW86V', partNumber: '9900003309' },
+            { product: 'MW74V', partNumber: '9900003311' },
+            { product: 'MW76V', partNumber: '9900003311' },
+            { product: 'MW86V', partNumber: '9900003311' },
+            { product: 'MW06V', partNumber: '9900003014' },
+            { product: 'MW06V', partNumber: '9900003013' },
+            { product: 'MW74V', partNumber: '9900003310' },
+            { product: 'MW76V', partNumber: '9900003310' },
+            { product: 'MW86V', partNumber: '9900003310' },
+            { product: 'MW74V', partNumber: '6800000749' },
+            { product: 'MW76V', partNumber: '6800000749' },
+            { product: 'MW86V', partNumber: '6800000926' },
+            { product: 'MW06V', partNumber: '6800000871' },
             { product: 'MW74V', partNumber: '9900002361' },
-            { product: 'MW74V', partNumber: '9900002428' },
-            { product: 'MW74V', partNumber: '9900002429' },
-            { product: 'MW74V', partNumber: '9900002430' },
-            { product: 'MW74V', partNumber: '9900002445' },
-            { product: 'MW74V', partNumber: '9900002504' }
+            { product: 'MW76V', partNumber: '9900002361' },
+            { product: 'MW86V', partNumber: '9900002361' },
+            { product: 'MW06V', partNumber: '9900002361' },
+            { product: 'MW06V', partNumber: '9900003019' },
+            { product: 'MW74V', partNumber: '6800000482' },
+            { product: 'MW76V', partNumber: '6800000482' },
+            { product: 'MW86V', partNumber: '6800000482' }
           ]}
         />
       </Container>
