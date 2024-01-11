4d0585585685776235ddcd3ceb8c9c9832468915
ronaldoTIrodrigues <rrodriguesdelimafilho@gmail.com>
2024-01-11 10:05:34 -0300

    featUpdateBranchesStockMach

--- src/pages/EstoquesMaq/index.js
+++ src/pages/EstoquesMaq/index.js
@@ -64,8 +64,6 @@ export default function EstoquesMaq() {
 
   const handleFilialPlaceholder = useCallback((filial) => {
     switch (filial) {
-      case ('0101', '0102', '0103', '0104', '0105', '0106'):
-        return 'Todas'
       case '0101':
         return 'Matriz'
       case '0102':
